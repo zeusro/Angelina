54fff5e587b9d15e7761c3cee8cc8686690f81f7
PPG <692486802@qq.com>
2018-09-30 15:38:17 +0800

    time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 $(function () {
 
 	var note = $('#note'),
-		newYear = true,
-		now = new Date();
+	    newYear = true;
+	var now = new Date();
 	var ts = new Date(now.getFullYear(), 8, 30);
 	if (now>ts) {
 		// The new year is here! Count towards something else.
 		// Notice the *1000 at the end - time must be in milliseconds
 		// ts = (new Date()).getTime() + 10*24*60*60*1000;
-		ts = new Date(ts.getFullYear() + 1, ts.getMonth(), ts.getDay());		
+		ts = new Date(ts.getFullYear() + 1, 8,30);		
 	}
 	console.log(ts);
 
